da5eb540dbb8ec10b55a7ea061b16ac3dcb595d3
kscotteng <kscott@gmail.com>
2016-02-24 01:45:38 -0500

    add association test to vote

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -8,5 +8,12 @@ class VoteTest < ActiveSupport::TestCase
   def test_vote_class_exists
     assert Vote
   end
-  
+
+  def test_vote_voter_id_candidate_id_and_candidate_association
+   v = Vote.create!(voter_id: 1, candidate_id: 1, candidate: "John Doe")
+   assert_equal 1, v.voter_id
+   assert_equal 1, v.candidate_id
+   assert_equal "John Doe", v.candidate
+ end
+
 end
