1255f4d49083fa4de1dc850eaee8ae147490d4b8
kscotteng <kscott@gmail.com>
2016-02-24 01:46:58 -0500

    add association test to voter

--- test/models/voter_test.rb
+++ test/models/voter_test.rb
@@ -8,5 +8,12 @@ class VoterTest < ActiveSupport::TestCase
   def test_voter_class_exists
     assert Voter
   end
+
+  def test_voter_name_party_and_token_association
+    vr = Voter.create!(name: "John Smith", party: "I", token: "555")
+    assert_equal "John Smith", vr.name
+    assert_equal "I", vr.party
+    assert_equal "555", vr.token
+  end
   
 end
