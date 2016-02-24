cca6b475975e38e80753db821ee17e5a39c864e9
kscotteng <kscott@gmail.com>
2016-02-24 01:42:54 -0500

    add association test to candidate

--- test/models/candidate_test.rb
+++ test/models/candidate_test.rb
@@ -8,5 +8,13 @@ class CandidateTest < ActiveSupport::TestCase
   def test_candidate_class_exists
     assert Candidate
   end
-  
+
+  def test_candidate_has_many_votes_association
+    c = Candidate.create!(name: "John Doe", hometown: "Greensboro", district: "Guilford", party: "I")
+    assert_equal "John Doe", c.name
+    assert_equal "Greensboro", c.hometown
+    assert_equal "Guilford", c.district
+    assert_equal "I", c.party
+  end
+
 end
