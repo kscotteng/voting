13b8e0e3cf386c5639fa9a948a827dc9684baa0d
kscotteng <kscott@gmail.com>
2016-02-24 13:39:44 -0500

    add candidates index action to return all candidates, add show action to return first candidate, add create action to create new candidate

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,10 +1,14 @@
 class CandidatesController < ApplicationController
   def index
+    render json: Candidates.all
   end
 
   def show
+    render json: Candidates.first
   end
 
   def create
+    c = Candidates.create(name: params[:name], hometown: params[:hometown], district: params[:district], party: params[:party])
+    render json: c
   end
 end
